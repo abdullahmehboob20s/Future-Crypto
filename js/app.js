3c8bf6249edb31c48e8e169a5434b781ccec6436
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-08-26 10:15:52 +0500

    roadmap done for mobile and desktop

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+const sidebar = document.querySelector(".nav-right");
+const crossBtn = document.querySelector(".cross-icon");
+const blackScreen = document.querySelector(".black-screen");
+
+function toggle() {
+  sidebar.classList.toggle("active");
+  blackScreen.classList.toggle("active");
+}
+
+hamburger.addEventListener("click", toggle);
+crossBtn.addEventListener("click", toggle);
+
+blackScreen.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  blackScreen.classList.remove("active");
+});
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   loop: true,
