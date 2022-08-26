3000653673fb13e27a7d7fe922d114f2da06d8f1
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-08-26 10:38:07 +0500

    Team section done for mobile

--- js/app.js
+++ js/app.js
@@ -28,4 +28,29 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    450: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    680: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 88,
+    },
+  },
 });
